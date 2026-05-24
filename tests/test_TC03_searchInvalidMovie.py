be908d6c5f6d15b0df02575ac77b9f711e380c48
Mala Sahu <malasahu.198@gmail.com>
2026-05-24 19:17:30 +0530

    git commit -m "update in tests"

--- tests/test_TC03_searchInvalidMovie.py
+++ tests/test_TC03_searchInvalidMovie.py
@@ -4,8 +4,3 @@ from pages.IMdbUIValidation import IMDbUIValidation
 def test_searchInvalidMovie(browser_instance):
     imdb_search = IMDbUIValidation(browser_instance)
     imdb_search.searchMovie("xyzabc123")
-
-
-def test_IMdbLogoClick(browser_instance):
-    imdb_search = IMDbUIValidation(browser_instance)
-    imdb_search.navigateToHomePage()
